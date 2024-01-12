459a6b4ce1eb83e3d2fe895b8c008cd36afedfe2
GAUTAM BHARDWAJ <131481375+gautambhardwaj-966@users.noreply.github.com>
2024-01-12 22:49:48 +0530

    Time: 56 ms (88.80%), Space: 45.3 MB (51.45%) - LeetHub

---
+++ 0135-candy/0135-candy.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    const n = ratings.length;
+    const candies = new Array(n).fill(1); // Initialize candies array with minimum value of 1 for each child
+
+    // Left to right pass
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    // Right to left pass
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    // Sum up the candies array to get the total minimum number of candies needed
+    return candies.reduce((total, num) => total + num, 0);
+
+};
