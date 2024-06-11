9d7e090c2243dc20ac43b617be48e0c80a43e5b9
GAUTAM BHARDWAJ <131481375+gautambhardwaj-966@users.noreply.github.com>
2024-06-11 23:16:47 +0530

    Time: 372 ms (11.86%), Space: 90.2 MB (32.45%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+// function memoize(fn) {
+    
+//     return function(...args) {
+        
+//     }
+// }
+
+
+
+function memoize(fn) {
+    var dit = {};
+    return function(...args) {
+        if( args in dit ) {
+            return dit[args];
+        } else {
+            dit[args] = fn(...args);
+            return dit[args];
+        }
+    }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
