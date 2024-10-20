16b232d6d9f2f10848bf8531cc43284de782be34
GAUTAM BHARDWAJ <131481375+gautambhardwaj-966@users.noreply.github.com>
2024-10-20 23:33:19 +0530

    Time: 51 ms (60.80%), Space: 49.1 MB (25.77%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -5,8 +5,8 @@
  */
 var map = function(arr, fn) {
     const result = [];
-    for(let i=0;i<arr.length;i++){
-        result.push(fn(arr[i], i));
+    for(i=0;i<=arr.length-1;i++){
+        result.push(fn(arr[i],i));
     }
     return result;
 };
