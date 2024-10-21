1bd33ffe0f9b5f1b259500db661ca7a271799231
GAUTAM BHARDWAJ <131481375+gautambhardwaj-966@users.noreply.github.com>
2024-10-21 21:06:43 +0530

    Time: 323 ms (25.57%), Space: 90.8 MB (17.13%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,23 +3,20 @@
  * @return {Function}
  */
 function memoize(fn) {
-  const cache = new Map(); // To store results
-  let callCount = 0; // To track the number of times fn is actually called
-
-  return function(...args) {
-    const key = JSON.stringify(args); // Serialize arguments as key for cache
-
-    // If the result for these arguments is in the cache, return it
-    if (cache.has(key)) {
-      return cache.get(key);
+    const cache = new Map();
+    let callCount = 0;
+    
+    return function(...args) {
+       const key = JSON.stringify(args);
+        
+        if(cache.has(key)){
+            return cache.get(key);
+        }
+        callCount++;
+        const result = fn(...args);
+        cache.set(key,result);
+        return result;
     }
-
-    // Otherwise, call the function and store the result in cache
-    callCount++;
-    const result = fn(...args);
-    cache.set(key, result); // Cache the result
-    return result;
-  };
 }
 
 
