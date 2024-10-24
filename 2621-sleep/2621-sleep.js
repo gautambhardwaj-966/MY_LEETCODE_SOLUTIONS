5e1fe425fec8bd4403c6310d8ade035f5875d371
GAUTAM BHARDWAJ <131481375+gautambhardwaj-966@users.noreply.github.com>
2024-10-24 23:38:55 +0530

    Time: 40 ms (97.69%), Space: 49.2 MB (8.74%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -3,12 +3,13 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(millis); // Resolve after `millis` milliseconds
-    }, millis);
-  });
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(millis);
+        },millis);
+    })
 }
+
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
